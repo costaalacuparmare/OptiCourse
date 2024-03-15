0f75aa6d0ef269e7fd0af8519e8bc8965a92d0c9
costaalacuparmare <vconstantinescu@protonmail.com>
2024-03-16 00:04:21 +0200

    Management System for Students and Courses in Java

---
+++ src/main/java/OptiCourse/DuplicateCourseException.java
@@ -0,0 +1,8 @@
+package OptiCourse;
+
+/* Exception thrown when a student is added twice to the list of students */
+public class DuplicateCourseException extends Exception {
+  public DuplicateCourseException(String message) {
+    super(message);
+  }
+}
---
+++ src/main/java/OptiCourse/DuplicateStudentException.java
@@ -0,0 +1,8 @@
+package OptiCourse;
+
+/* Exception thrown when a student is added twice to the list of students */
+public class DuplicateStudentException extends Exception {
+  public DuplicateStudentException(String message) {
+    super(message);
+  }
+}
